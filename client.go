d4c30b94ddccdf8cdab58bd54d02f68084a1c1ac
Lang Martin <lang@hashicorp.com>
2019-12-30 20:28:43 -0500

    client move details out to other files

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/rand"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
@@ -13,7 +14,7 @@ import (
 type ClientConfig struct {
 	ID          string
 	Addr        string
-	Port        int
+	Boot        string
 	RootPEMFile string
 }
 
@@ -34,7 +35,13 @@ type Client struct {
 	out    []h.Message
 }
 
-func MakeClient(c *ClientConfig) (*Client, err) {
+func makeID() string {
+	bs := make([]byte, 8)
+	rand.Read(bs)
+	return fmt.Sprintf("%160x", bs)
+}
+
+func MakeClient(c *ClientConfig) (*Client, error) {
 	addr := fmt.Sprintf("%s:%d", c.Addr, c.Port)
 	cert, err := ioutil.ReadFile(c.RootPEMFile)
 	if err != nil {
@@ -144,47 +151,4 @@ func (c *Client) failActive(peer *h.Node) error {
 		}
 	}
 	return nil
-}
-
-func (c *Client) GossipSend(payload int) {
-	for c.app.Hot > 0 {
-		if h.Rint(c.world.config.gossipHeat) > c.app.Hot {
-			continue
-		}
-
-		peer := c.hv.Peer()
-		if peer == nil {
-			c.failActive(nil) // ignore errors
-			continue
-		}
-
-		hot, err := c.gossipSend(peer, payload)
-		if err != nil {
-			c.failActive(peer)
-		}
-
-		if !hot {
-			c.app.Hot -= 1
-		}
-	}
-}
-
-func (c *Client) gossipSend(peer *h.Node, payload int) (bool, error) {
-	bs, err := c.sendSync(peer, []byte{payload})
-	var out bool
-	if len(bs) > 0 {
-		out = bs[0] == 1
-	}
-	return out, err
-}
-
-func (c *Client) GossipRecv(payload int) bool {
-	if c.app.Value >= payload {
-		c.app.Waste += 1
-		return false
-	}
-	c.app.Value = payload
-	c.app.Seen += 1
-	c.app.Hot = c.world.config.gossipHeat
-	return true
 }
