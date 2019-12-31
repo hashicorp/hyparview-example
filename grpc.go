a036d2eb16630ae393b2cbd4599912e93bd6525e
Lang Martin <lang@hashicorp.com>
2019-12-30 20:27:06 -0500

    grpc wrap requests

--- grpc.go
+++ grpc.go
@@ -2,4 +2,73 @@
 
 package main
 
-type server struct{}
+import (
+	"context"
+
+	h "github.com/hashicorp/hyparview"
+	"github.com/hashicorp/hyparview-example/proto/proto"
+)
+
+type server struct {
+	c *Client
+}
+
+func newServer(c *Client) *server {
+	return &server{c: c}
+}
+
+func (s *server) RecvGossip(ctx context.Context, req *proto.GossipRequest) (*proto.GossipResponse, error) {
+	hot := s.c.gossipRecv(req.payload)
+	return hot, nil
+}
+
+func (s *server) RecvJoin(ctx context.Context, req *proto.JoinRequest) (*proto.Empty, error) {
+	to, from := s.c.hv.Self, &h.Node{Addr: req.from}
+	ms := s.c.hv.RecvJoin(h.SendJoin(to, from))
+	s.c.Outbox(ms...)
+	return &proto.Empty{}, nil
+}
+
+func (s *server) RecvForwardJoin(ctx context.Context, req *proto.ForwardJoinRequest) (*proto.Empty, error) {
+	to, from := s.c.hv.Self, &h.Node{Addr: req.from}
+	join := &h.Node{Addr: req.join}
+	ttl := req.ttl
+	ms := s.c.hv.RecvForwardJoin(h.SendForwardJoin(to, from, join, ttl))
+	s.c.Outbox(ms...)
+	return &proto.Empty{}, nil
+}
+
+func (s *server) RecvDisconnect(ctx context.Context, req *proto.FromRequest) (*proto.Empty, error) {
+	to, from := s.c.hv.Self, &h.Node{Addr: req.from}
+	s.c.hv.RecvDisconnect(h.SendDisconnect(to, from))
+	return &proto.Empty{}, nil
+}
+
+func (s *server) RecvNeighbor(ctx context.Context, req *proto.NeighborRequest) (*proto.NeighborResponse, error) {
+	to, from := s.c.hv.Self, &h.Node{Addr: req.From}
+	priority := req.Priority
+	ms := s.c.hv.RecvNeighbor(h.SendNeighbor(to, from, priority))
+	accept := len(ms) == 0
+	return &proto.NeighborResponse{Accept: accept}, nil
+}
+
+func (s *server) RecvShuffle(ctx context.Context, req *proto.ShuffleRequest) (*proto.ShuffleReply, error) {
+	to, from := s.c.hv.Self, &h.Node{Addr: req.From}
+	active := req.Active
+	passive := req.Passive
+	ttl := req.Ttl
+	ms := s.c.hv.RecvShuffle(h.SendShuffle(to, from, active, passive, ttl))
+
+	var res *proto.ShuffleReply
+
+	for _, m := range ms {
+		switch v := m.(type) {
+		case h.SendShuffle:
+			s.c.Outbox(m)
+		case h.ShuffleReply:
+			res = &proto.ShuffleReply{Passive: v.Passive}
+		}
+	}
+
+	return res, nil
+}
--- main.go
+++ main.go
@@ -1,10 +1,41 @@
 package main
 
-var Hyparview := h.
+import (
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
 
 // Example is the main entry point
-func Example() {
-	view := 
-	
+func main() {
+	c, _ := MakeClient(&ClientConfig{
+		ID:          makeID(),
+		Addr:        os.Getenv("LISTEN"),
+		Boot:        os.Getenv("BOOTSTRAP"),
+		RootPEMFile: os.Getenv("ROOT_PEM"),
+	})
 
+	go c.lpShuffle()
+	go c.joinCluster()
+	server(c)
+}
+
+func server(c *Client) {
+	lis, err := net.Listen("tcp", c.config.Addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	creds, err := credentials.NewServerTLSFromFile(c.config.RootPEMFile, c.config.KeyFile)
+	if err != nil {
+		log.Fatalf("Failed to generate credentials %v", err)
+	}
+
+	opts = []grpc.ServerOption{grpc.Creds(creds)}
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterRouteGuideServer(grpcServer, newServer(c))
+	grpcServer.Serve(lis)
 }
