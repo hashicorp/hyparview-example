3cb1e442d87a7738dc21305514e5755812fe9441
Lang Martin <lang@hashicorp.com>
2019-12-30 20:31:01 -0500

    proto hyparview rebuild

--- proto/proto/hyparview.pb.go
+++ proto/proto/hyparview.pb.go
@@ -33,7 +33,7 @@ func (m *Empty) Reset()         { *m = Empty{} }
 func (m *Empty) String() string { return proto.CompactTextString(m) }
 func (*Empty) ProtoMessage()    {}
 func (*Empty) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hyparview_a162a67d0fb79508, []int{0}
+	return fileDescriptor_hyparview_045416e7d88fb9d4, []int{0}
 }
 func (m *Empty) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Empty.Unmarshal(m, b)
@@ -53,9 +53,48 @@ func (m *Empty) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Empty proto.InternalMessageInfo
 
+type FromRequest struct {
+	From                 string   `protobuf:"bytes,1,opt,name=from,proto3" json:"from,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *FromRequest) Reset()         { *m = FromRequest{} }
+func (m *FromRequest) String() string { return proto.CompactTextString(m) }
+func (*FromRequest) ProtoMessage()    {}
+func (*FromRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_hyparview_045416e7d88fb9d4, []int{1}
+}
+func (m *FromRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_FromRequest.Unmarshal(m, b)
+}
+func (m *FromRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_FromRequest.Marshal(b, m, deterministic)
+}
+func (dst *FromRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_FromRequest.Merge(dst, src)
+}
+func (m *FromRequest) XXX_Size() int {
+	return xxx_messageInfo_FromRequest.Size(m)
+}
+func (m *FromRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_FromRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_FromRequest proto.InternalMessageInfo
+
+func (m *FromRequest) GetFrom() string {
+	if m != nil {
+		return m.From
+	}
+	return ""
+}
+
 type ForwardJoinRequest struct {
 	Ttl                  int32    `protobuf:"varint,1,opt,name=ttl,proto3" json:"ttl,omitempty"`
 	Join                 string   `protobuf:"bytes,2,opt,name=join,proto3" json:"join,omitempty"`
+	From                 string   `protobuf:"bytes,3,opt,name=from,proto3" json:"from,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -65,7 +104,7 @@ func (m *ForwardJoinRequest) Reset()         { *m = ForwardJoinRequest{} }
 func (m *ForwardJoinRequest) String() string { return proto.CompactTextString(m) }
 func (*ForwardJoinRequest) ProtoMessage()    {}
 func (*ForwardJoinRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hyparview_a162a67d0fb79508, []int{1}
+	return fileDescriptor_hyparview_045416e7d88fb9d4, []int{2}
 }
 func (m *ForwardJoinRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_ForwardJoinRequest.Unmarshal(m, b)
@@ -99,8 +138,16 @@ func (m *ForwardJoinRequest) GetJoin() string {
 	return ""
 }
 
+func (m *ForwardJoinRequest) GetFrom() string {
+	if m != nil {
+		return m.From
+	}
+	return ""
+}
+
 type NeighborRequest struct {
 	Priority             bool     `protobuf:"varint,1,opt,name=priority,proto3" json:"priority,omitempty"`
+	From                 string   `protobuf:"bytes,2,opt,name=from,proto3" json:"from,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -110,7 +157,7 @@ func (m *NeighborRequest) Reset()         { *m = NeighborRequest{} }
 func (m *NeighborRequest) String() string { return proto.CompactTextString(m) }
 func (*NeighborRequest) ProtoMessage()    {}
 func (*NeighborRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hyparview_a162a67d0fb79508, []int{2}
+	return fileDescriptor_hyparview_045416e7d88fb9d4, []int{3}
 }
 func (m *NeighborRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_NeighborRequest.Unmarshal(m, b)
@@ -137,8 +184,16 @@ func (m *NeighborRequest) GetPriority() bool {
 	return false
 }
 
+func (m *NeighborRequest) GetFrom() string {
+	if m != nil {
+		return m.From
+	}
+	return ""
+}
+
 type NeighborResponse struct {
 	Accept               bool     `protobuf:"varint,1,opt,name=accept,proto3" json:"accept,omitempty"`
+	From                 string   `protobuf:"bytes,2,opt,name=from,proto3" json:"from,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -148,7 +203,7 @@ func (m *NeighborResponse) Reset()         { *m = NeighborResponse{} }
 func (m *NeighborResponse) String() string { return proto.CompactTextString(m) }
 func (*NeighborResponse) ProtoMessage()    {}
 func (*NeighborResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hyparview_a162a67d0fb79508, []int{3}
+	return fileDescriptor_hyparview_045416e7d88fb9d4, []int{4}
 }
 func (m *NeighborResponse) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_NeighborResponse.Unmarshal(m, b)
@@ -175,10 +230,18 @@ func (m *NeighborResponse) GetAccept() bool {
 	return false
 }
 
+func (m *NeighborResponse) GetFrom() string {
+	if m != nil {
+		return m.From
+	}
+	return ""
+}
+
 type ShuffleRequest struct {
 	Ttl                  int32    `protobuf:"varint,1,opt,name=ttl,proto3" json:"ttl,omitempty"`
 	Active               []string `protobuf:"bytes,2,rep,name=active,proto3" json:"active,omitempty"`
 	Passive              []string `protobuf:"bytes,3,rep,name=passive,proto3" json:"passive,omitempty"`
+	From                 string   `protobuf:"bytes,4,opt,name=from,proto3" json:"from,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -188,7 +251,7 @@ func (m *ShuffleRequest) Reset()         { *m = ShuffleRequest{} }
 func (m *ShuffleRequest) String() string { return proto.CompactTextString(m) }
 func (*ShuffleRequest) ProtoMessage()    {}
 func (*ShuffleRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hyparview_a162a67d0fb79508, []int{4}
+	return fileDescriptor_hyparview_045416e7d88fb9d4, []int{5}
 }
 func (m *ShuffleRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_ShuffleRequest.Unmarshal(m, b)
@@ -229,8 +292,16 @@ func (m *ShuffleRequest) GetPassive() []string {
 	return nil
 }
 
+func (m *ShuffleRequest) GetFrom() string {
+	if m != nil {
+		return m.From
+	}
+	return ""
+}
+
 type ShuffleReply struct {
 	Passive              []string `protobuf:"bytes,1,rep,name=passive,proto3" json:"passive,omitempty"`
+	From                 string   `protobuf:"bytes,2,opt,name=from,proto3" json:"from,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -240,7 +311,7 @@ func (m *ShuffleReply) Reset()         { *m = ShuffleReply{} }
 func (m *ShuffleReply) String() string { return proto.CompactTextString(m) }
 func (*ShuffleReply) ProtoMessage()    {}
 func (*ShuffleReply) Descriptor() ([]byte, []int) {
-	return fileDescriptor_hyparview_a162a67d0fb79508, []int{5}
+	return fileDescriptor_hyparview_045416e7d88fb9d4, []int{6}
 }
 func (m *ShuffleReply) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_ShuffleReply.Unmarshal(m, b)
@@ -267,8 +338,16 @@ func (m *ShuffleReply) GetPassive() []string {
 	return nil
 }
 
+func (m *ShuffleReply) GetFrom() string {
+	if m != nil {
+		return m.From
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*Empty)(nil), "hashicorp.hyparview.example.hyparview.Empty")
+	proto.RegisterType((*FromRequest)(nil), "hashicorp.hyparview.example.hyparview.FromRequest")
 	proto.RegisterType((*ForwardJoinRequest)(nil), "hashicorp.hyparview.example.hyparview.ForwardJoinRequest")
 	proto.RegisterType((*NeighborRequest)(nil), "hashicorp.hyparview.example.hyparview.NeighborRequest")
 	proto.RegisterType((*NeighborResponse)(nil), "hashicorp.hyparview.example.hyparview.NeighborResponse")
@@ -288,12 +367,11 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type HyparviewClient interface {
-	RecvJoin(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*Empty, error)
+	RecvJoin(ctx context.Context, in *FromRequest, opts ...grpc.CallOption) (*Empty, error)
 	RecvForwardJoin(ctx context.Context, in *ForwardJoinRequest, opts ...grpc.CallOption) (*Empty, error)
-	RecvDisconnect(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*Empty, error)
+	RecvDisconnect(ctx context.Context, in *FromRequest, opts ...grpc.CallOption) (*Empty, error)
 	RecvNeighbor(ctx context.Context, in *NeighborRequest, opts ...grpc.CallOption) (*NeighborResponse, error)
 	RecvShuffle(ctx context.Context, in *ShuffleRequest, opts ...grpc.CallOption) (*ShuffleReply, error)
-	RecvShuffleReply(ctx context.Context, in *ShuffleReply, opts ...grpc.CallOption) (*Empty, error)
 }
 
 type hyparviewClient struct {
@@ -304,7 +382,7 @@ func NewHyparviewClient(cc *grpc.ClientConn) HyparviewClient {
 	return &hyparviewClient{cc}
 }
 
-func (c *hyparviewClient) RecvJoin(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*Empty, error) {
+func (c *hyparviewClient) RecvJoin(ctx context.Context, in *FromRequest, opts ...grpc.CallOption) (*Empty, error) {
 	out := new(Empty)
 	err := c.cc.Invoke(ctx, "/hashicorp.hyparview.example.hyparview.Hyparview/RecvJoin", in, out, opts...)
 	if err != nil {
@@ -322,7 +400,7 @@ func (c *hyparviewClient) RecvForwardJoin(ctx context.Context, in *ForwardJoinRe
 	return out, nil
 }
 
-func (c *hyparviewClient) RecvDisconnect(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*Empty, error) {
+func (c *hyparviewClient) RecvDisconnect(ctx context.Context, in *FromRequest, opts ...grpc.CallOption) (*Empty, error) {
 	out := new(Empty)
 	err := c.cc.Invoke(ctx, "/hashicorp.hyparview.example.hyparview.Hyparview/RecvDisconnect", in, out, opts...)
 	if err != nil {
@@ -349,23 +427,13 @@ func (c *hyparviewClient) RecvShuffle(ctx context.Context, in *ShuffleRequest, o
 	return out, nil
 }
 
-func (c *hyparviewClient) RecvShuffleReply(ctx context.Context, in *ShuffleReply, opts ...grpc.CallOption) (*Empty, error) {
-	out := new(Empty)
-	err := c.cc.Invoke(ctx, "/hashicorp.hyparview.example.hyparview.Hyparview/RecvShuffleReply", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
 // HyparviewServer is the server API for Hyparview service.
 type HyparviewServer interface {
-	RecvJoin(context.Context, *Empty) (*Empty, error)
+	RecvJoin(context.Context, *FromRequest) (*Empty, error)
 	RecvForwardJoin(context.Context, *ForwardJoinRequest) (*Empty, error)
-	RecvDisconnect(context.Context, *Empty) (*Empty, error)
+	RecvDisconnect(context.Context, *FromRequest) (*Empty, error)
 	RecvNeighbor(context.Context, *NeighborRequest) (*NeighborResponse, error)
 	RecvShuffle(context.Context, *ShuffleRequest) (*ShuffleReply, error)
-	RecvShuffleReply(context.Context, *ShuffleReply) (*Empty, error)
 }
 
 func RegisterHyparviewServer(s *grpc.Server, srv HyparviewServer) {
@@ -373,7 +441,7 @@ func RegisterHyparviewServer(s *grpc.Server, srv HyparviewServer) {
 }
 
 func _Hyparview_RecvJoin_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Empty)
+	in := new(FromRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -385,7 +453,7 @@ func _Hyparview_RecvJoin_Handler(srv interface{}, ctx context.Context, dec func(
 		FullMethod: "/hashicorp.hyparview.example.hyparview.Hyparview/RecvJoin",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(HyparviewServer).RecvJoin(ctx, req.(*Empty))
+		return srv.(HyparviewServer).RecvJoin(ctx, req.(*FromRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -409,7 +477,7 @@ func _Hyparview_RecvForwardJoin_Handler(srv interface{}, ctx context.Context, de
 }
 
 func _Hyparview_RecvDisconnect_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Empty)
+	in := new(FromRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -421,7 +489,7 @@ func _Hyparview_RecvDisconnect_Handler(srv interface{}, ctx context.Context, dec
 		FullMethod: "/hashicorp.hyparview.example.hyparview.Hyparview/RecvDisconnect",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(HyparviewServer).RecvDisconnect(ctx, req.(*Empty))
+		return srv.(HyparviewServer).RecvDisconnect(ctx, req.(*FromRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -462,24 +530,6 @@ func _Hyparview_RecvShuffle_Handler(srv interface{}, ctx context.Context, dec fu
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Hyparview_RecvShuffleReply_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(ShuffleReply)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(HyparviewServer).RecvShuffleReply(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/hashicorp.hyparview.example.hyparview.Hyparview/RecvShuffleReply",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(HyparviewServer).RecvShuffleReply(ctx, req.(*ShuffleReply))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
 var _Hyparview_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "hashicorp.hyparview.example.hyparview.Hyparview",
 	HandlerType: (*HyparviewServer)(nil),
@@ -504,41 +554,38 @@ var _Hyparview_serviceDesc = grpc.ServiceDesc{
 			MethodName: "RecvShuffle",
 			Handler:    _Hyparview_RecvShuffle_Handler,
 		},
-		{
-			MethodName: "RecvShuffleReply",
-			Handler:    _Hyparview_RecvShuffleReply_Handler,
-		},
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "proto/hyparview.proto",
 }
 
-func init() { proto.RegisterFile("proto/hyparview.proto", fileDescriptor_hyparview_a162a67d0fb79508) }
+func init() { proto.RegisterFile("proto/hyparview.proto", fileDescriptor_hyparview_045416e7d88fb9d4) }
 
-var fileDescriptor_hyparview_a162a67d0fb79508 = []byte{
-	// 371 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x54, 0x4d, 0x4b, 0xc3, 0x30,
-	0x18, 0x5e, 0x37, 0xbb, 0x8f, 0xd7, 0xb1, 0x8d, 0x80, 0x52, 0x7a, 0x1a, 0x01, 0xa1, 0x88, 0x56,
-	0x70, 0xa8, 0xe8, 0x51, 0x54, 0xc4, 0x83, 0x87, 0xea, 0xc9, 0x5b, 0x57, 0x33, 0x1b, 0xe9, 0x9a,
-	0x98, 0x66, 0x9b, 0x45, 0x2f, 0xfe, 0x50, 0xff, 0x8b, 0x24, 0x6b, 0xf7, 0xa1, 0x08, 0xad, 0xe0,
-	0xa9, 0x79, 0x9b, 0xe7, 0x23, 0x7d, 0xf2, 0x50, 0xd8, 0xe2, 0x82, 0x49, 0x76, 0x10, 0xa6, 0xdc,
-	0x17, 0x53, 0x4a, 0x66, 0xae, 0x9e, 0xd1, 0x4e, 0xe8, 0x27, 0x21, 0x0d, 0x98, 0xe0, 0xee, 0x72,
-	0x8b, 0xbc, 0xfa, 0x63, 0x1e, 0x91, 0xe5, 0x1b, 0xdc, 0x00, 0xf3, 0x72, 0xcc, 0x65, 0x8a, 0xcf,
-	0x00, 0x5d, 0x31, 0x31, 0xf3, 0xc5, 0xe3, 0x0d, 0xa3, 0xb1, 0x47, 0x5e, 0x26, 0x24, 0x91, 0xa8,
-	0x07, 0x35, 0x29, 0x23, 0xcb, 0xe8, 0x1b, 0x8e, 0xe9, 0xa9, 0x25, 0x42, 0xb0, 0xf1, 0xcc, 0x68,
-	0x6c, 0x55, 0xfb, 0x86, 0xd3, 0xf2, 0xf4, 0x1a, 0xef, 0x43, 0xf7, 0x96, 0xd0, 0xa7, 0x70, 0xc8,
-	0x44, 0x4e, 0xb4, 0xa1, 0xc9, 0x05, 0x65, 0x82, 0xca, 0x54, 0xb3, 0x9b, 0xde, 0x62, 0xc6, 0xbb,
-	0xd0, 0x5b, 0xc2, 0x13, 0xce, 0xe2, 0x84, 0xa0, 0x6d, 0xa8, 0xfb, 0x41, 0x40, 0xb8, 0xcc, 0xd0,
-	0xd9, 0x84, 0xef, 0xa1, 0x73, 0x17, 0x4e, 0x46, 0xa3, 0x88, 0xfc, 0x7e, 0x24, 0xcd, 0x95, 0x74,
-	0x4a, 0xac, 0x6a, 0xbf, 0xe6, 0xb4, 0xbc, 0x6c, 0x42, 0x16, 0x34, 0xb8, 0x9f, 0x24, 0x6a, 0xa3,
-	0xa6, 0x37, 0xf2, 0x11, 0x3b, 0xd0, 0x5e, 0xa8, 0xf2, 0x28, 0x5d, 0x45, 0x1a, 0x6b, 0xc8, 0xc3,
-	0x4f, 0x13, 0x5a, 0xd7, 0x79, 0x5a, 0x28, 0x84, 0xa6, 0x47, 0x82, 0xa9, 0x4a, 0x08, 0xed, 0xb9,
-	0x85, 0x12, 0x76, 0x75, 0xbc, 0x76, 0x29, 0x34, 0xae, 0xa0, 0x77, 0xe8, 0x2a, 0xa7, 0x95, 0x2b,
-	0x41, 0xa7, 0x05, 0x25, 0x7e, 0x5e, 0x63, 0x69, 0xf7, 0x18, 0x3a, 0xca, 0xfd, 0x82, 0x26, 0x01,
-	0x8b, 0x63, 0x12, 0xc8, 0x7f, 0xfe, 0xda, 0x0f, 0x03, 0xda, 0xca, 0x30, 0xaf, 0x05, 0x3a, 0x2e,
-	0x28, 0xf0, 0xad, 0x76, 0xf6, 0x49, 0x69, 0xde, 0xbc, 0x7f, 0xb8, 0x82, 0xde, 0x60, 0x53, 0x1d,
-	0x21, 0xeb, 0x05, 0x3a, 0x2a, 0xa8, 0xb4, 0xde, 0x4e, 0x7b, 0x50, 0x96, 0xc6, 0x23, 0x15, 0xc0,
-	0x0c, 0x7a, 0x2b, 0xe6, 0xf3, 0x52, 0xfe, 0x45, 0xaa, 0x6c, 0xf2, 0xe7, 0x8d, 0x07, 0x53, 0xff,
-	0x2f, 0x86, 0x75, 0xfd, 0x18, 0x7c, 0x05, 0x00, 0x00, 0xff, 0xff, 0xfb, 0xee, 0x4e, 0x7a, 0x4f,
-	0x04, 0x00, 0x00,
+var fileDescriptor_hyparview_045416e7d88fb9d4 = []byte{
+	// 398 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xbc, 0x94, 0x4f, 0x4b, 0xeb, 0x40,
+	0x14, 0xc5, 0x9b, 0xa6, 0x7f, 0x6f, 0x4b, 0x5b, 0x06, 0xde, 0x23, 0x64, 0xd5, 0x17, 0x78, 0xd0,
+	0xc5, 0x23, 0x0f, 0x5a, 0x54, 0x04, 0x11, 0x14, 0x2d, 0xe2, 0xa2, 0x8b, 0xb8, 0x73, 0x97, 0xc6,
+	0xa9, 0x19, 0x49, 0x32, 0xe3, 0x64, 0xda, 0x1a, 0x74, 0xe3, 0x37, 0xf6, 0x23, 0xc8, 0x4c, 0x93,
+	0x34, 0x6a, 0x85, 0x74, 0xe3, 0xaa, 0xf7, 0xf6, 0xce, 0xf9, 0x9d, 0x19, 0xee, 0x21, 0xf0, 0x8b,
+	0x71, 0x2a, 0xe8, 0x7f, 0x3f, 0x61, 0x2e, 0x5f, 0x11, 0xbc, 0xb6, 0x55, 0x8f, 0xfe, 0xfa, 0x6e,
+	0xec, 0x13, 0x8f, 0x72, 0x66, 0x6f, 0x47, 0xf8, 0xc9, 0x0d, 0x59, 0x80, 0xb7, 0xff, 0x58, 0x4d,
+	0xa8, 0x5f, 0x86, 0x4c, 0x24, 0xd6, 0x1f, 0xe8, 0x4c, 0x39, 0x0d, 0x1d, 0xfc, 0xb8, 0xc4, 0xb1,
+	0x40, 0x08, 0x6a, 0x0b, 0x4e, 0x43, 0x43, 0x1b, 0x6a, 0xa3, 0xb6, 0xa3, 0x6a, 0x6b, 0x06, 0x68,
+	0x4a, 0xf9, 0xda, 0xe5, 0x77, 0xd7, 0x94, 0x44, 0xd9, 0xc9, 0x01, 0xe8, 0x42, 0x04, 0xea, 0x60,
+	0xdd, 0x91, 0xa5, 0xd4, 0x3e, 0x50, 0x12, 0x19, 0xd5, 0x8d, 0x56, 0xd6, 0x39, 0x4f, 0x2f, 0xf0,
+	0xce, 0xa0, 0x3f, 0xc3, 0xe4, 0xde, 0x9f, 0x53, 0x9e, 0xc1, 0x4c, 0x68, 0x31, 0x4e, 0x28, 0x27,
+	0x22, 0x51, 0xc4, 0x96, 0x93, 0xf7, 0x39, 0xa2, 0x5a, 0x40, 0x9c, 0xc2, 0x60, 0x8b, 0x88, 0x19,
+	0x8d, 0x62, 0x8c, 0x7e, 0x43, 0xc3, 0xf5, 0x3c, 0xcc, 0x44, 0x4a, 0x48, 0xbb, 0x9d, 0x7a, 0x1f,
+	0x7a, 0x37, 0xfe, 0x72, 0xb1, 0x08, 0xf0, 0xf7, 0xcf, 0x51, 0x3c, 0x41, 0x56, 0xd8, 0xa8, 0x0e,
+	0xf5, 0x51, 0xdb, 0x49, 0x3b, 0x64, 0x40, 0x93, 0xb9, 0x71, 0x2c, 0x07, 0xba, 0x1a, 0x64, 0x6d,
+	0xee, 0x54, 0x2b, 0x38, 0x9d, 0x40, 0x37, 0x77, 0x62, 0x41, 0x52, 0x54, 0x6b, 0xbb, 0xd5, 0x85,
+	0x7b, 0x8e, 0xdf, 0x6a, 0xd0, 0xbe, 0xca, 0x96, 0x86, 0x22, 0x68, 0x39, 0xd8, 0x5b, 0xc9, 0x2d,
+	0xa0, 0xb1, 0x5d, 0x6a, 0xd1, 0x76, 0x61, 0xb9, 0xe6, 0xbf, 0x92, 0x9a, 0x4d, 0x32, 0x2a, 0xe8,
+	0x05, 0xfa, 0xd2, 0xaf, 0xb0, 0x7c, 0x74, 0x5c, 0xd6, 0xf6, 0x4b, 0x60, 0xf6, 0x76, 0x17, 0xd0,
+	0x93, 0xee, 0x17, 0x24, 0xf6, 0x68, 0x14, 0x61, 0x4f, 0xfc, 0xc8, 0x9b, 0x5f, 0x35, 0xe8, 0x4a,
+	0xdb, 0x2c, 0x5e, 0xe8, 0xb0, 0x24, 0xe0, 0x53, 0xa4, 0xcd, 0xa3, 0xbd, 0x75, 0x9b, 0x1c, 0x5b,
+	0x15, 0xf4, 0x0c, 0x1d, 0x79, 0x85, 0x34, 0x37, 0xe8, 0xa0, 0x24, 0xe9, 0x63, 0xa2, 0xcd, 0xc9,
+	0xbe, 0x32, 0x16, 0x24, 0x56, 0xe5, 0xbc, 0x79, 0x5b, 0x57, 0x5f, 0x92, 0x79, 0x43, 0xfd, 0x4c,
+	0xde, 0x03, 0x00, 0x00, 0xff, 0xff, 0x78, 0x82, 0x47, 0xcf, 0x69, 0x04, 0x00, 0x00,
 }
