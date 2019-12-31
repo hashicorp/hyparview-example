9b9a599e14643dafb051ea2a682bf6447842a882
Lang Martin <lang@hashicorp.com>
2019-12-31 15:14:34 -0500

    new util with []string <-> []*Node

---
+++ util.go
@@ -0,0 +1,17 @@
+package main
+
+import h "github.com/hashicorp/hyparview"
+
+func sliceNodeAddr(ns []*h.Node) (ss []string) {
+	for _, n := range ns {
+		ss = append(ss, n.Addr)
+	}
+	return ss
+}
+
+func sliceAddrNode(ss []string) (ns []*h.Node) {
+	for _, n := range ss {
+		ns = append(ns, &h.Node{Addr: n})
+	}
+	return ns
+}
