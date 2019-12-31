fdaa89f4604eab6cef45fd421373ca62116da5ac
Lang Martin <lang@hashicorp.com>
2019-12-31 15:15:59 -0500

    main start gprc server, join client

--- main.go
+++ main.go
@@ -5,37 +5,41 @@ import (
 	"net"
 	"os"
 
+	"github.com/hashicorp/hyparview-example/proto/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
 // Example is the main entry point
 func main() {
-	c, _ := MakeClient(&ClientConfig{
-		ID:          makeID(),
+	c, _ := newClient(&clientConfig{
+		ID:          newID(),
 		Addr:        os.Getenv("LISTEN"),
 		Boot:        os.Getenv("BOOTSTRAP"),
 		RootPEMFile: os.Getenv("ROOT_PEM"),
+		keyFile:     os.Getenv("SERVER_KEY_PEM"),
 	})
 
 	go c.lpShuffle()
 	go c.joinCluster()
-	server(c)
+	runServer(c)
 }
 
-func server(c *Client) {
+func runServer(c *client) {
 	lis, err := net.Listen("tcp", c.config.Addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	creds, err := credentials.NewServerTLSFromFile(c.config.RootPEMFile, c.config.KeyFile)
+	creds, err := credentials.NewServerTLSFromFile(c.config.RootPEMFile, c.config.keyFile)
 	if err != nil {
 		log.Fatalf("Failed to generate credentials %v", err)
 	}
+	opts := []grpc.ServerOption{grpc.Creds(creds)}
 
-	opts = []grpc.ServerOption{grpc.Creds(creds)}
 	grpcServer := grpc.NewServer(opts...)
-	pb.RegisterRouteGuideServer(grpcServer, newServer(c))
+	srv := newServer(c)
+	proto.RegisterHyparviewServer(grpcServer, srv)
+	proto.RegisterGossipServer(grpcServer, srv)
 	grpcServer.Serve(lis)
 }
