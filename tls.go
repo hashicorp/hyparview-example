86b9c8afadecaf934f255d928d4ec6db75c55584
Lang Martin <lang@hashicorp.com>
2020-01-05 22:22:23 -0500

    new tls does the more complex server & client setup

---
+++ tls.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"io/ioutil"
+
+	"google.golang.org/grpc/credentials"
+)
+
+func serverCreds(c *clientConfig) (credentials.TransportCredentials, error) {
+	certificate, err := tls.LoadX509KeyPair(c.serverCert, c.serverKey)
+	if err != nil {
+		return nil, fmt.Errorf("could not load server key pair: %s", err)
+	}
+
+	certPool, err := caPool(c.caCert)
+	if err != nil {
+		return nil, err
+	}
+
+	creds := credentials.NewTLS(&tls.Config{
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{certificate},
+		ClientCAs:    certPool,
+	})
+
+	return creds, nil
+}
+
+func caPool(caCert string) (*x509.CertPool, error) {
+	// Create a certificate pool from the certificate authority
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile(c.caCert)
+	if err != nil {
+		return nil, fmt.Errorf("could not read ca certificate: %s", err)
+	}
+
+	// Append the client certificates from the CA
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		return nil, errors.New("failed to append client certs")
+	}
+
+	return certPool, nil
+}
+
+func clientCreds(c *clientConfig, addr string) (credentials.TransportCredentials, error) {
+	certificate, err := tls.LoadX509KeyPair(c.clientCert, c.clientKey)
+	if err != nil {
+		return nil, fmt.Errorf("could not load client key pair: %s", err)
+	}
+
+	certPool, err := caPool(c.caCert)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the TLS credentials
+	creds := credentials.NewTLS(&tls.Config{
+		ServerName:   addr,
+		Certificates: []tls.Certificate{certificate},
+		RootCAs:      certPool,
+	})
+
+	return creds, nil
+}
