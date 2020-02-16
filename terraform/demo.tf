8ef0338585d42164afb8b7720ec353d1b0993c15
Lang Martin <lang@hashicorp.com>
2020-02-16 14:31:01 -0500

    terraform load an ssh key (ssh still isn't running)

--- terraform/demo.tf
+++ terraform/demo.tf
@@ -17,19 +17,20 @@ data "aws_ami" "amazon-linux-2" {
   }
 }
 
+resource "aws_key_pair" "demo-key" {
+  key_name = "demo-key"
+  public_key = file("demo-key.pub")
+}
+
 resource "aws_instance" "demo" {
   ami                         = data.aws_ami.amazon-linux-2.id
   instance_type               = "t3.medium"
   associate_public_ip_address = true
+  key_name                    = "demo-key"
 
-  count = 5
+  count = 2
   tags = {
     Name = "hyparview-demo-${count.index}"
-  }
-
-
-  provisioner "remote-exec" {
-
   }
 }
 
