b2578ae7b5a90a65e57fca21acc72d63caeb70fe
Lang Martin <lang@hashicorp.com>
2020-02-16 16:39:11 -0500

    terraform/demo: need a security group, I guess

--- terraform/demo.tf
+++ terraform/demo.tf
@@ -22,13 +22,39 @@ resource "aws_key_pair" "demo-key" {
   public_key = file("demo-key.pub")
 }
 
+resource "aws_security_group" "hyparview-demo-sg" {
+  name   = "hyparview-demo-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "demo" {
   ami                         = data.aws_ami.amazon-linux-2.id
   instance_type               = "t3.medium"
   associate_public_ip_address = true
   key_name                    = "demo-key"
+  security_groups             = ["hyparview-demo-sg"]
 
-  count = 2
+  count = 5
   tags = {
     Name = "hyparview-demo-${count.index}"
   }
