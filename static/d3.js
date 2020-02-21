cb100ced74e1630635e9d05724e5d1da2b1543d1
Lang Martin <lang@hashicorp.com>
2020-02-21 17:15:18 -0500

    static/d3: missing links

--- static/d3.js
+++ static/d3.js
@@ -21,16 +21,28 @@
         linkSet: {},
     };
 
-    fetch.every = 5000;
-    fetch.then = function (resp) {
-        updateGraph(svg, simulation, data, resp);
-    };
-
-    fetch.loop();
+    refreshTask(5000);
 
     function updateGraph(svg, sim, data, resp) {
-        setwiseAdd(data.nodes, data.nodeSet, nodeKey, resp.nodes);
-        setwiseAdd(data.links, data.linkSet, linkKey, resp.links);
+        // links point to node objects
+        var key, x, del;
+        var add = setwiseAdds(data.nodeSet, nodeKey, resp.nodes);
+        for (key in add) {
+            x = add[key];
+            x.x = randInt(width);
+            x.y = randInt(height);
+            data.nodes.push(x);
+            data.nodeSet[nodeKey(x)] = x;
+        }
+        setwiseDel(data.nodes, data.nodeSet, nodeKey, add);
+
+        add = setwiseAdds(data.linkSet, linkKey, resp.links);
+        for (key in add) {
+            x = add[key];
+            x.source = data.nodeSet[x.source];
+            x.target = data.nodeSet[x.target];
+        }
+
         const ns = updateNodes(svg, data.nodes);
         const ls = updateLinks(svg, data.links);
         updateSimulation(sim, ns, ls);
@@ -52,102 +64,76 @@
     }
 
     function updateNodes(svg, nodes) {
-        const ng = svg.append("g").attr("class", "node");
-        let ns = ng.selectAll("circle").data(nodes, nodeKey);
+        let ns = svg.selectAll("circle").data(nodes, nodeKey);
         ns.exit().remove();
-        const enter = ns.enter().append()
-              .join("circle")
+
+        const enter = ns.enter()
+              .append("circle")
+              .attr("stroke", "#fff")
+              .attr("stroke-width", 1.5)
               .attr("r", 5)
-              .attr("fill", color);
+              .attr("fill", "#000");
         return ns;
     }
 
     function updateLinks(svg, links) {
-        const lg = svg.append("g").attr("class", "link");
-        let ls = lg.selectAll("line").data(links, linkKey);
+        let ls = svg.selectAll("line").data(links, linkKey);
         ls.exit().remove();
-        const enter = ls.enter().append()
+
+        const enter = ls.enter()
+              .append("line")
               .attr("stroke", "#999")
               .attr("stroke-opacity", 0.6);
         ls = enter.merge(ls);
         return ls;
     }
 
-    // https://observablehq.com/@d3/force-directed-graph
-    // https://medium.com/ninjaconcept/interactive-dynamic-force-directed-graphs-with-d3-da720c6d7811
-    function chart(config, data) {
-        const width = config.width;
-        const height = config.height;
+    // ==================================================
+    // Fetch a bunch of stuff
 
-        var simulation = d3.forceSimulation(data.nodes)
-            .force("link", d3.forceLink(data.links).id(d => d.id))
-            .force("charge", d3.forceManyBody())
-            .force("center", d3.forceCenter(width / 2, height / 2));
-
-        var svg = d3.create("svg")
-            .attr("viewBox", [0, 0, width, height]);
-
-        var link = svg.append("g")
-            .attr("stroke", "#999")
-            .attr("stroke-opacity", 0.6)
-            .selectAll("line")
-            .data(data.links)
-            .enter().append('line')
-            .join("line")
-            .attr("stroke-width", d => Math.sqrt(d.value));
-        link.exit().remove();
-
-        var node = svg.append("g")
-            .attr("stroke", "#fff")
-            .attr("stroke-width", 1.5)
-            .selectAll("circle")
-            .data(data.nodes)
-            .enter().append("circle")
-            .join("circle")
-            .attr("r", 5)
-            .attr("fill", color);
-        node.exit().remove();
-        // node.append("title").text(d => d.id);
-
-        simulation.on("tick", () => {
-            link
-                .attr("x1", d => d.source.x)
-                .attr("y1", d => d.source.y)
-                .attr("x2", d => d.target.x)
-                .attr("y2", d => d.target.y);
-
-            node
-                .attr("cx", d => d.x)
-                .attr("cy", d => d.y);
-        });
-
-        // invalidation.then(() => simulation.stop());
-        return svg.node();
+    function randInt(n, m) {
+        return Math.floor(Math.random() * (m - n + 1)) + n;
     }
 
-    function color(node) {
-        const scale = d3.scaleOrdinal(d3.schemeCategory10);
-        return d => scale(d.group);
+    function handle(text) {
+        var resp = JSON.parse(text);
+        updateGraph(svg, simulation, data, resp);
+    }
+
+    function refresh() {
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", function () {
+            handle(this.responseText);
+        });
+        xhr.open("GET", "d3");
+        xhr.send();
+    }
+
+    function refreshTask(n) {
+        refresh();
+        setTimeout(function () {refreshTask(n);}, n);
     }
 
     // ==================================================
     // Only add new items, remove missing ones
 
-    function setwiseAdd(data, set, keyf, xs) {
-        var xss = {},
-            del = {};
-
+    function setwiseAdds(set, keyf, xs) {
+        var xss = {};
         // add new elements, populate xss
         xs.forEach(function (x) {
             var k = keyf(x);
             xss[k] = true;
             if (!set[k]) {
-                data.push(x);
                 set[k] = true;
             }
         });
 
-        // use xss to find delete keys
+        return xss;
+    }
+
+    function setwiseDel(data, set, keyf, xss) {
+        var del = {};
+
         for (let k in set) {
             if (!xss[k]) {
                 del[k] = true;
@@ -163,7 +149,9 @@
         return n.id;
     }
 
-    function linkKey(x) {
-        return x.source + x.target;
+    function linkKey(l) {
+        var s = (typeof l.source == "string") ? l.source : l.source.id,
+            t = (typeof l.target == "string") ? l.target : l.target.id;
+        return s + t;
     }
 })(d3, fetch);
