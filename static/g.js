220873275b27dc2088d6c6799057ea94357db716
Lang Martin <lang@hashicorp.com>
2020-02-23 13:08:43 -0500

    static/g: seems to work!

--- static/g.js
+++ static/g.js
@@ -7,24 +7,62 @@ const elem = document.getElementById("graph");
 
 const Graph = ForceGraph()(elem)
       .onNodeHover(node => elem.style.cursor = node ? 'pointer' : null)
-      .onNodeClick(removeNode)
+      .nodeAutoColorBy('app')
       .graphData(initData);
 
-setInterval(() => {
-    const { nodes, links } = Graph.graphData();
-    const id = nodes.length;
-    Graph.graphData({
-        nodes: [...nodes, { id }],
-        links: [...links, { source: id, target: Math.round(Math.random() * (id-1)) }]
+setInterval(refresh, 1000);
+
+function refresh() {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", () => {
+        var resp = JSON.parse(xhr.responseText);
+        updateGraph(resp);
     });
-}, 1000);
+    xhr.open("GET", "d3");
+    xhr.send();
+}
 
-//
+function updateGraph(resp) {
+    const data = mergeData(Graph.graphData(), resp);
+    Graph.graphData(data);
+}
 
-function removeNode(node) {
-    let { nodes, links } = Graph.graphData();
-    links = links.filter(l => l.source !== node && l.target !== node); // Remove links attached to node
-    nodes.splice(node.id, 1); // Remove node
-    nodes.forEach((n, idx) => { n.id = idx; }); // Reset node ids to array index
-    Graph.graphData({ nodes, links });
+// create new arrays of nodes, links. We're using object identity to
+// see of a thing has changed, so we want to preserve the old object
+// between refreshes
+function mergeData(data, resp) {
+    var node_ids = {};
+    var nodes = data.nodes.map(n => {
+        if (! resp.nodes[n.id]) return null; // missing
+        node_ids[n.id] = true;               // mark the idp
+        if (nodeEq(n, resp.nodes[n.id])) return n; // keep old obj
+        return resp.nodes[n.id];
+    })
+        .filter(n => { return n; });
+
+    for (var k in resp.nodes) {
+        if (node_ids[k]) continue;
+        nodes.push(resp.nodes[k]);
+    }
+
+    var link_ids = {};
+    const links = data.links.map(l => {
+        const id = l.source + l.target;
+        if (! resp.links[id]) return null; // missing
+        link_ids[id] = true;
+        return l;               // updates don't matter
+    }).filter(l => { return l; });
+
+    for (k in resp.links) {
+        if (link_ids[k]) continue;
+        links.push(resp.links[k]);
+    }
+
+    function nodeEq(n, o) {
+        return n && o &&
+            n.id == o.id &&
+            n.app == o.app;
+    }
+
+    return { nodes, links };
 }
