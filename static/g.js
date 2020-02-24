139384f25e97c2646a0174b5d7fd3a1cd3521e48
Lang Martin <lang@hashicorp.com>
2020-02-24 14:21:37 -0500

    static/g: try to change the color in place

--- static/g.js
+++ static/g.js
@@ -40,9 +40,10 @@ function mergeData(data, resp) {
     var node_ids = {};
     var nodes = data.nodes.map(n => {
         if (! resp.nodes[n.id]) return null; // missing
-        node_ids[n.id] = true;               // mark the idp
-        if (nodeEq(n, resp.nodes[n.id])) return n; // keep old obj
-        return resp.nodes[n.id];
+        node_ids[n.id] = true;               // mark the id
+        // if (! nodeEq(n, resp.nodes[n.id])) return resp.nodes[n.id];
+        n.app = resp.nodes[n.id].app;
+        return n; // keep old obj
     })
         .filter(n => { return n; });
 
